ad91506a883863e51fd4e369f6df3d78fb31a748
ben blazak <benblazak.dev@gmail.com>
2025-04-09 12:40:11 -0700

    audio

--- sound_monitor/audio/audio.py
+++ sound_monitor/audio/audio.py
@@ -15,37 +15,40 @@ _logger = logging.getLogger(__name__)
 
 
 class AudioBlock:
-    _resample_yamnet_rate = 16000  # YAMNet requires 16kHz input
+    # yamnet resample rate
+    # - eg up:down = 1:3 for 16k:48k
+    _resample_yamnet_rate = 16000  # yamnet requires 16khz input
     _resample_source_rate = _config.uma8_sample_rate
     _resample_g = gcd(_resample_yamnet_rate, _resample_source_rate)
     _resample_up = _resample_yamnet_rate // _resample_g
     _resample_down = _resample_source_rate // _resample_g
 
+    # bandpass filter
+    _bandpass_nyquist = _config.uma8_sample_rate / 2
+    _bandpass_low, _bandpass_high = _config.audio_bandpass_filter
+    _bandpass_sos = butter(
+        4, # filter order
+        [_bandpass_low / _bandpass_nyquist, _bandpass_high / _bandpass_nyquist],
+        btype="band",
+        output="sos",
+    )
+
     def __init__(self, data: np.ndarray, time: float) -> None:
         self.raw_data: np.ndarray = data  # shape (block_size, channels)
         self.timestamp = datetime.fromtimestamp(time)
 
-        # create bandpass filter
-        nyquist = _config.uma8_sample_rate / 2
-        low, high = _config.audio_bandpass_filter
-        self._sos = butter(
-            4, [low / nyquist, high / nyquist], btype="band", output="sos"
-        )
-
         # yamnet data: mono, filtered, resampled to 16khz
         mono_channel = _config.audio_mono_channel
         mono_data = data[:, mono_channel]
-        filtered_mono = sosfilt(self._sos, mono_data)
-        # Use the pre-calculated ratios from Audio singleton
-        audio = Audio.get()
+        filtered_mono = sosfilt(self._bandpass_sos, mono_data)
         self.yamnet_data = resample_poly(
             filtered_mono, self._resample_up, self._resample_down
-        )  # e.g., 1:3 for 16k:48k
+        )
 
         # direction data: all 7 channels, filtered
         self.direction_data = np.zeros_like(data)
         for ch in range(data.shape[1]):
-            self.direction_data[:, ch] = sosfilt(self._sos, data[:, ch])
+            self.direction_data[:, ch] = sosfilt(self._bandpass_sos, data[:, ch])
 
         # recording data: just extract the stereo channels
         left, right = _config.audio_stereo_channels
