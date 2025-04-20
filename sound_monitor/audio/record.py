d34775079ee0a565e0d1e8bf28f0e8864f3bbeff
ben blazak <benblazak.dev@gmail.com>
2025-04-19 17:09:49 -0700

    record

--- sound_monitor/audio/record.py
+++ sound_monitor/audio/record.py
@@ -25,13 +25,16 @@ class Record:
         path: Path | None = None,
         time: datetime | None = None,
     ) -> None:
+        if self._process is not None:
+            return
+
         self.time = time or datetime.now()
         self.path = path or _config.data_dir / _config.prefix(time) + ".mp3"
         self._process = subprocess.Popen(
             _config.record_ffmpeg_command_partial + [self.path],
             stdin=subprocess.PIPE,
             stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
+            stderr=subprocess.PIPE,
         )
 
         for block in _input.buffer:
@@ -44,10 +47,17 @@ class Record:
         )
 
     def stop(self) -> None:
+        if self._process is None:
+            return
+
         _input.remove_callback(f"record-{id(self)}")
 
         self._process.stdin.close()
-        self._process.wait()
+        ret = self._process.wait()
+        if ret:
+            _logger.error(f"ffmpeg exited with code {ret}")
+            _logger.error(self._process.stderr.read().decode())
+            # TODO might want to email this
 
     def _callback(self, input: Input) -> None:
         block = input.buffer[-1]
